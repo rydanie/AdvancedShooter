23131170af859b7914cca4b6e3f66f38964df60b
rydanie <rydanielson18@saintjosephhs.com>
2016-05-04 08:44:59 -0400

    started hero

--- RyleyScrollShooter/src/MetalGameHero.java
+++ RyleyScrollShooter/src/MetalGameHero.java
@@ -1,16 +1,56 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class MetalGameHero implements Runnable, Player, GameObject {
 
-	public MetalGameHero(){
+	String name1 = "MHero_Front.jpg";
+	BufferedImage img;
+	int sizeP =20;
+	int originX;
+	int originY;
+	Rectangle hitBox;
+	
+	public MetalGameHero(Point p){
+		
+		//File name = new File("MHero_Front.jpg");
+		//name1 = name;
+		
+		loadImage(name1);
+		
+		setThisObjectLocation(p);
+		
+		hitBox = new Rectangle();
+		
+		System.out.println("I Loaded");
+	}
+	
+	public void loadImage(String s){
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File(s));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
+	public void setImage(String s){
+		
+		name1 = s;
+	}
 	
 	@Override
 	public void setThisObjectLocation(Point p) {
 		// TODO Auto-generated method stub
+		originX = p.x;
+		originY = p.y;
 		
 	}
 
@@ -23,7 +63,30 @@ public class MetalGameHero implements Runnable, Player, GameObject {
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
-		
+		Graphics2D g2d = (Graphics2D)g;
+        
+        //fillout = ColorPanel.fo;
+        /*
+        if(c != null){
+	        g2d.setColor( c );
+		 }else{
+			 g2d.setColor( Color.BLACK );
+		 }
+		 */
+        //g2d.clearRect( originX, originY, sizeX, sizeY );  // this is cool to make a background-filled rectangle!
+        /*
+        if(fillout == 2){
+			g.drawRect(originX, originY, sizeX, sizeY );
+		}else if(fillout == 1){
+			g.fillRect(originX, originY, sizeX, sizeY);
+		}
+        */
+       //R = new Rectangle(originX, originY, sizeX, sizeY);
+       g.drawImage(img, originX, originY, sizeP, sizeP, null);
+        
+        
+        System.out.println( "Redrawing Image @" + 20 + ", " + 20 + "; " + 20 + " x " + 20);
+        //this.setSize( this.getPreferredSize() );
 	}
 
 	@Override
